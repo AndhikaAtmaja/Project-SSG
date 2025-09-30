aa00be96a5e90adf04c48ef550fdee57dd7dedbf
ZerosPen <andhikaatmaja51@gmail.com>
2025-09-30 11:47:14 +0700

    Update

--- Assets/Script/Phone/Application/PhoneApplicationData/ApplicationSO.cs
+++ Assets/Script/Phone/Application/PhoneApplicationData/ApplicationSO.cs
@@ -6,6 +6,6 @@ using UnityEngine.UI;
 [CreateAssetMenu(menuName = ("ApplicationSO"), fileName = "NewApplicationData")]
 public class ApplicationSO : ScriptableObject
 {
-    public Image ApplicationIcon;
+    public Sprite ApplicationIcon;
     public string ApplicationName;
 }
