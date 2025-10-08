861a3403bf4546eee44f8bf050e1ac16bad4135d
ZerosPen <137518772+ZerosPen@users.noreply.github.com>
2025-10-08 09:28:25 +0700

    update

--- Assets/Script/Helper/CommentButtonHandler.cs
+++ Assets/Script/Helper/CommentButtonHandler.cs
@@ -7,14 +7,22 @@ public class CommentButtonHandler : MonoBehaviour
 {
     [SerializeField] private GameObject commentSection;
     [SerializeField] private ScrollRect mainScroll;
-    private bool isCommentOpen;
+    [SerializeField] private bool isCommentOpen;
+
+    private void Start()
+    {
+        if (mainScroll == null)
+        {
+            mainScroll = GameObject.FindWithTag("MainScroll").GetComponent<ScrollRect>();
+        }
+    }
 
     public void OnToggleComment()
     {
         isCommentOpen = !isCommentOpen;
 
         if (mainScroll != null)
-            mainScroll.enabled = isCommentOpen;
+            mainScroll.enabled = !isCommentOpen;
 
         if (commentSection != null)
             commentSection.SetActive(isCommentOpen);
